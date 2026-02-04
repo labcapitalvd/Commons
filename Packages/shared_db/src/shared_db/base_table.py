b4b05c708fc74ce0804312af338b37b78947674b
SpanishSyntax <SpanishSyntax@cuspide.club>
2026-02-04 15:37:26 -0500

    uuid_utils

--- Packages/shared_db/src/shared_db/base_table.py
+++ Packages/shared_db/src/shared_db/base_table.py
@@ -1,5 +1,5 @@
 from uuid import UUID
-from uuid-utils import uuid7
+from uuid_utils import uuid7
 from enum import Enum
 from typing import cast
 
@@ -7,12 +7,14 @@ from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy.dialects.postgresql import UUID as UUIDType
 
+
 class Base(DeclarativeBase):
     id: Mapped[UUID] = mapped_column(
         UUIDType(as_uuid=True),
         primary_key=True,
         default=uuid7,
     )
+
 
 class BaseTargetTable(Enum):
     def __init__(self, table_name: str, schema: str):
@@ -22,6 +24,7 @@ class BaseTargetTable(Enum):
     @property
     def fq_name(self) -> str:
         return f"{self.schema}.{self.table}"
+
 
 def merge_enums(name, *enums) -> type[BaseTargetTable]:
     members = {}
@@ -37,6 +40,8 @@ def merge_enums(name, *enums) -> type[BaseTargetTable]:
     MergedEnumIter = cast(type[BaseTargetTable], MergedEnum)
 
     for member in MergedEnumIter:
-        assert hasattr(member, "table") and hasattr(member, "schema"), f"{member.name} missing table/schema"
+        assert hasattr(member, "table") and hasattr(member, "schema"), (
+            f"{member.name} missing table/schema"
+        )
 
     return MergedEnumIter
