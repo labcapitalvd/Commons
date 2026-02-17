6b159b31ab0cfa2cda56dcd12b716320e6ab70e8
SpanishSyntax <SpanishSyntax@cuspide.club>
2026-02-17 14:40:22 -0500

    cambio de node a private

--- Auth/src/main.py
+++ Auth/src/main.py
@@ -14,21 +14,29 @@ from shared_schemas import (
 )
 from shared_utils import configure_logging, get_logger
 
+from domain.services.auth.errors import (
+    AuthError,
+    InvalidCredentials,
+    TierDoesntExist,
+    TokenRevoked,
+    UserAlreadyExists,
+    UserDisabled,
+)
+from domain.services.token.errors import TokenError, TokenExpired
+from utils.files import FileError, FileNameError
+
 from routers.auth import router as router_auth
 from routers.files import router as router_files
 
 configure_logging()
 
 VERSION = "0.1.0"
-PRODUCTION_MODE = os.getenv("PRODUCTION_MODE", "false").lower() in (
-    "1", "true", "yes"
-)
-
+PRODUCTION_MODE = os.getenv("PRODUCTION_MODE", "false").lower() in ("1", "true", "yes")
 COOKIES_SECURE = False if not PRODUCTION_MODE else True
 
 
 PUBLIC = os.getenv("PUBLIC_ORIGINS", "*")
-NODE = os.environ["NODE_ORIGINS"]
+PRIVATE = os.environ["PRIVATE_ORIGINS"]
 
 logger = get_logger(__name__)
 
@@ -37,8 +45,8 @@ if not PRODUCTION_MODE:
     PUBLIC_METHODS = ["*"]
     PUBLIC_HEADERS = ["*"]
 
-    NODE_ORIGINS = ["*"]
-    NODE_ALLOWED_HOSTS = ["*"]
+    PRIVATE_ORIGINS = ["*"]
+    PRIVATE_ALLOWED_HOSTS = ["*"]
 
 else:
     PUBLIC_ORIGINS = [item.strip() for item in PUBLIC.split(",") if item.strip()]
@@ -53,8 +61,8 @@ else:
         "Access-Control-Request-Headers",
         "X-Platform",
     ]
-    NODE_ORIGINS = [item.strip() for item in NODE.split(",") if item.strip()]
-    NODE_ALLOWED_HOSTS = ["frontend"]
+    PRIVATE_ORIGINS = [item.strip() for item in PRIVATE.split(",") if item.strip()]
+    PRIVATE_ALLOWED_HOSTS = ["frontend"]
 
 ##############################################################################################
 # Context Manager
@@ -77,9 +85,19 @@ api = FastAPI(
     lifespan=lifespan,
 )
 
-api_node = FastAPI(
-    title="Servicios privados para hub",
-    description="API para hub",
+api_private = FastAPI(
+    title="API privada.",
+    description="""API PRIVADA para consumo de la data del HUB.
+    Aquí encontrará todos los métodos que la Veeduría Distrital pone a su disposición para la consulta de los datos relacionados con el HUB.
+
+Algunos de los métodos disponibles son:
+
+    - Obtener información sobre los resultados en el HUB de una entidad Distrital.
+    - Consultar las respuestas dadas por las entidades distritales.
+    - Comparar historicos de resultados en el HUB de una entidad Distrital.
+
+Tenga en cuenta que algunos endpoints están protegidos por autenticación y autorización. Por favor escribanos a labcapital@veeduriadistrital.gov.co para obtener más información.
+""",
     version=VERSION,
     lifespan=lifespan,
     openapi_tags=[
@@ -88,8 +106,8 @@ api_node = FastAPI(
 )
 
 api_public = FastAPI(
-    title="Endpoints publicos para data del HUB",
-    description="""API para consumo de la data del HUB.
+    title="API publica.",
+    description="""API PUBLICA para consumo de la data del HUB.
     Aquí encontrará todos los métodos que la Veeduría Distrital pone a su disposición para la consulta de los datos relacionados con el HUB.
 
 Algunos de los métodos disponibles son:
@@ -116,11 +134,11 @@ Tenga en cuenta que algunos endpoints están protegidos por autenticación y aut
 
 # TrustedHostMiddleware must be added before CORSMiddleware so that CORS is the outer-most
 # layer and handles OPTIONS requests correctly.
-api_node.add_middleware(TrustedHostMiddleware, allowed_hosts=NODE_ALLOWED_HOSTS)
+api_private.add_middleware(TrustedHostMiddleware, allowed_hosts=PRIVATE_ALLOWED_HOSTS)
 
-api_node.add_middleware(
+api_private.add_middleware(
     CORSMiddleware,
-    allow_origins=NODE_ORIGINS,
+    allow_origins=PRIVATE_ORIGINS,
     allow_credentials=COOKIES_SECURE,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -137,75 +155,32 @@ api_public.add_middleware(
 )
 
 ##############################################################################################
-# ExceptionHandlerMiddleware
-##############################################################################################
-
-api_node.add_exception_handler(CustomError, custom_error_handler)
-api_public.add_exception_handler(CustomError, custom_error_handler)
-
-##############################################################################################
-# ExceptionHandlerMiddleware
-##############################################################################################
-
-
-@api_node.exception_handler(RequestValidationError)
-async def validation_exception_handler_frontend(
-    request: Request, exc: RequestValidationError
-):
-    errors = [
-        ItemError(
-            code="INVALID_REQUEST",
-            message=e["msg"],
-            more_info=".".join(map(str, e["loc"])),
-        )
-        for e in exc.errors()
-    ]
-    return await custom_error_handler(request, CustomError(errors=errors))
-
-
-@api_public.exception_handler(RequestValidationError)
-async def validation_exception_handler(request: Request, exc: RequestValidationError):
-    errors = [
-        ItemError(
-            code="INVALID_REQUEST",
-            message=e["msg"],
-            more_info=".".join(map(str, e["loc"])),
-        )
-        for e in exc.errors()
-    ]
-    return await custom_error_handler(request, CustomError(errors=errors))
-
-
-##############################################################################################
 # LOGS
 ##############################################################################################
 
 logger.info(f"Client app CORS origins: {PUBLIC_ORIGINS}")
 logger.info(f"Client app allow_credentials: {COOKIES_SECURE}")
-logger.info(f"Node app CORS origins: {NODE_ORIGINS}")
+logger.info(f"Node app CORS origins: {PRIVATE_ORIGINS}")
 logger.info(f"Node app allow_credentials: {COOKIES_SECURE}")
 
 ##############################################################################################
 # Montaje de frontend en la aplicación principal
 ##############################################################################################
 
-api.mount("/node", api_node)
+api.mount("/node", api_private)
 api.mount("/public", api_public)
 
 ##############################################################################################
 # Montaje de frontend en la aplicación principal
 ##############################################################################################
-public_routers = [
-    router_auth,
-    router_files
-]
+public_routers = [router_auth, router_files]
 
-node_routers = []
+private_routers = []
 
-all_routers = public_routers + node_routers
+all_routers = public_routers + private_routers
 
-add_routers_with_custom_errors(api_node, all_routers)
-for route in api_node.routes:
+add_routers_with_custom_errors(api_private, all_routers)
+for route in api_private.routes:
     logger.info(f"Public route mounted: {route}")
 
 add_routers_with_custom_errors(api_public, public_routers)
