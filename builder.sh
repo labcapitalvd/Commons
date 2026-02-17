80dcd0f8144a6f6e0ea9ad089f81775e82680cd5
SpanishSyntax <SpanishSyntax@cuspide.club>
2026-02-17 15:38:57 -0500

    cachebusters

--- builder.sh
+++ builder.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
-if [[ "$1" == "cachebuster" ]]; then
-    CACHEBUST=$(date +%s)
+if [[ "$1" == "fetcher" ]]; then
+    FETCHER=$(date +%s)
     shift
 else
-    CACHEBUST=1
+    FETCHER=1
 fi
 
-cd Auth && docker compose build --build-arg CACHEBUST=$CACHEBUST "$@" && docker push labcapital/apps:Auth
+if [[ "$1" == "packages" ]]; then
+    PACKAGES=$(date +%s)
+    shift
+else
+    PACKAGES=1
+fi
+
+cd Auth && docker compose build --build-arg FETCHER=$FETCHER "$@" --build-arg PACKAGES=$PACKAGES "$@" && docker push labcapital/apps:Auth
